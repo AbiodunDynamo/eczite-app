1f9ce99ea2b40d224cc6d9b77a60e7750aad9b7c
onifs10 <onifadebolu64@gmail.com>
2020-12-07 19:33:19 +0100

    done with home page :smiley: :wink:

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/eczite/style.css">
     <link rel="stylesheet" href="/eczite/home.css">
-    <title>Title</title>
+    <title>Eczite - Home</title>
 </head>
 <body class="main-body">
 <header class="header">
